2425f7d264606b4b5297b22b40d37eedd4ca55b6
GitHub Action <action@github.com>
2024-12-05 11:32:09 +0800

    Auto Update 2024-12-05 11:32

--- surge/scripts/Weibo.js
+++ surge/scripts/Weibo.js
@@ -1,5 +1,5 @@
 // 引用地址：https://kelee.one/Resource/Script/Weibo/Weibo_remove_ads.js
-// 更新时间：2024-12-04 20:40:08
+// 更新时间：2024-12-05 09:40:26
 /*
 引用地址：https://raw.githubusercontent.com/RuCu6/Loon/main/Scripts/weibo.js
 */
